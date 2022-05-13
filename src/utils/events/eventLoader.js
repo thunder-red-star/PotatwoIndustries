05896da8882e46b882ce8433ec7bede2bfbb7cfd
ThunderRedStar <wvongphanith30@stuy.edu>
2022-05-12 21:31:58 -0400

    event loader fix

--- src/utils/events/eventLoader.js
+++ src/utils/events/eventLoader.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const Chalk = require('chalk');
 
 module.exports = (client) => {
-	let events = fs.readdirSync('./src/utils/events/');
+	let events = fs.readdirSync('./src/events/');
 	for (let i = 0; i < events.length; i++) {
 		if (!events[i].endsWith('.js')) {
 			return;
