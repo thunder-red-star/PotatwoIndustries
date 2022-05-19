d2b8862af06a90ee297f6207cabb1feaec6f3127
wvongphanith30 <thunder@stuy.edu>
2022-05-19 13:31:39 -0400

    testing modular server prefix get

--- src/utils/parsers/getServerPrefix.js
+++ src/utils/parsers/getServerPrefix.js
@@ -1,10 +1,10 @@
 module.exports = function(message) {
     // Get server prefix function
     let prefix;
-    let serverData = client.database.servers.get(message.guild.id);
+    let serverData = message.client.database.servers.get(message.guild.id);
     if (serverData && serverData !== null) {
         prefix = serverData.getPrefix();
     } else {
-        prefix = client.config.defaultPrefix;
+        prefix = message.client.config.defaultPrefix;
     }
 }
