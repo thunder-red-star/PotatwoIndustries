f39c6934062def19ac7c7292d77aa4aa271958b2
wvongphanith30 <thunder@stuy.edu>
2022-05-19 13:39:20 -0400

    help cmd

--- src/commands/message/info/help.js
+++ src/commands/message/info/help.js
@@ -1,6 +1,7 @@
 // Bot ping command.
 const messagePaginator = require("../../../utils/design/messagePaginator");
 const fs = require("fs");
+const ms = require("ms");
 const DJSBuilders = require("@discordjs/builders");
 
 module.exports = {
@@ -16,14 +17,15 @@ module.exports = {
     cooldown: 1000,
     args: [
         {
-            name: "query",
-            description: "The command or category to get help for.",
+            name: "command",
+            description: "The command to get help for.",
             type: "string",
             required: false
         }
     ],
     run: async function(message, client, args) {
         // If no query is provided, send a list of commands.
+        let serverPrefix = client.getServerPrefix(message);
         if (!args.query) {
             // Scan directory of messageCommands for commands.
             let paginatorEmbeds = [];
@@ -33,11 +35,11 @@ module.exports = {
                 let commandsInModule = fs.readdirSync(`./src/commands/message/${modules[i]}`).filter(file => file.endsWith(".js"));
                 let moduleEmbed = new DJSBuilders.Embed()
                     .setTitle(`${upperCaseModule} Commands`)
-                    .setDescription("You can use `" + client.getServerPrefix(message) + "help <command>` to get more information about a command.");
+                    .setDescription("You can use `" + serverPrefix + "help <command>` to get more information about a command.");
                 for (let j = 0; j < commandsInModule.length; j++) {
                     let command = require(`../../../commands/message/${modules[i]}/${commandsInModule[j]}`);
                     moduleEmbed.addField({
-                        name: `${client.config.defaultPrefix}${command.name}`,
+                        name: `\`${serverPrefix}${command.name}\``,
                         value: command.description,
                         inline: false
                     });
@@ -45,6 +47,59 @@ module.exports = {
                 paginatorEmbeds.push(moduleEmbed);
             }
             await messagePaginator(message, paginatorEmbeds);
+        } else {
+            // Find the command.
+            let command = client.messageCommands.find(command => command.name === args.query || command.messageCommandAliases.includes(args.query));
+            if (!command) {
+                return message.reply({
+                    embeds: [{
+                        color: client.colors.error,
+                        description: client.customEmojis.error + " I couldn't find a command with that name."
+                    }]
+                });
+            } else {
+                // Send the command's help message.
+                let cmdArgString = "";
+                for (let i = 0; i < command.args.length; i++) {
+                    if (i > 0) cmdArgString += " ";
+                    if (command.args[i].required) {
+                        cmdArgString += "<" + command.args[i].name + ">";
+                    } else {
+                        cmdArgString += "[" + command.args[i].name + "]";
+                    }
+                }
+                let helpEmbed = new DJSBuilders.Embed()
+                    .setTitle(`${serverPrefix}${command.name}`)
+                    .setDescription(command.detailedDescription)
+                    .setColor(client.colors.success)
+                    .addField({
+                        name: "Usage",
+                        value: `\`${serverPrefix}${command.name} ${cmdArgString}\``,
+                    })
+                    .addField({
+                        name: "Aliases",
+                        value: command.aliases.length > 0 ? command.aliases.map(alias => `\`${serverPrefix}${alias}\``).join(", ") : "None",
+                    })
+                    .addField({
+                        name: "Cooldown",
+                        value: `${ms(command.cooldown)}`,
+                    })
+                    .addField({
+                        name: "Permissions Required",
+                        value: command.userPermissions.length > 0 ? command.userPermissions.map(perm => `\`${perm}\``).join(", ") : "None",
+                    })
+                    .addField({
+                        name: "Bot Permissions Required",
+                        value: command.botPermissions.length > 0 ? command.botPermissions.map(perm => `\`${perm}\``).join(", ") : "None",
+                    })
+                    .addField({
+                        name: "Other command info",
+                        value: `Enabled: ${command.enabled ? "Yes" : "No"}\nGuild Only: ${command.guildOnly ? "Yes" : "No"}\nOwner Only: ${command.ownerOnly ? "Yes" : "No"}`
+                    });
+                message.reply({
+                    embeds: [helpEmbed]
+                }
+            }
         }
     }
 }
