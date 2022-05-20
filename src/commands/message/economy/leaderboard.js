6e2fa2762d8419bcbd5307fac319c1c5d5b1b384
wvongphanith30 <tankishootermk7@gmail.com>
2022-05-20 08:49:41 -0400

    testing

---
+++ src/commands/message/economy/leaderboard.js
@@ -0,0 +1,47 @@
+// Bot say command.
+const messagePaginator = require("../../../utils/design/messagePaginator");
+const DJSBuilders = require("@discordjs/builders");
+let randomArrayElement = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+};
+
+module.exports = {
+    botPermissions: ["SEND_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "USE_EXTERNAL_EMOJIS"],
+    userPermissions: [],
+    enabled: true,
+    guildOnly: false,
+    ownerOnly: false,
+    name: "leaderboard",
+    aliases: ["lb"],
+    description: "Get the leaderboard.",
+    detailedDescription: "Get and display the people with the most potatoes.",
+    cooldown: 2000,
+    args: [],
+    run: async function(message, client, args) {
+        // Retrieve the users
+        let users = await client.database.users.getUsers();
+        // Sort the users by their balance
+        users.sort((a, b) => {
+            return b.count - a.count;
+        });
+
+        // Create the leaderboard
+        let leaderboardPaginatorPages = [];
+        // Parse up to 100 users, 10 per page.
+        for (let i = 0; i < Math.ceil(users.length / 10); i++) {
+            let leaderboardPage = new DJSBuilders.Embed()
+                .setColor(client.colors.potato)
+                .setTitle("Leaderboard");
+            let description = "";
+            // Add up to 10 users to the page, but if there are less than 10 users, add all of them.
+            for (let j = 0; j < Math.min(10, users.length - i * 10); j++) {
+                let user = users[i * 10 + j];
+                description += `**${j + 1}**. ${user.name} - ${user.count} ${client.customEmojis.potato}\n`;
+            }
+            leaderboardPage.setDescription(description);
+            leaderboardPaginatorPages.push(leaderboardPage);
+        }
+        // Send the leaderboard
+        await messagePaginator(message, leaderboardPaginatorPages);
+    }
+};
