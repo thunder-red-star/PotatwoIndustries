0762fa6c4b20034407b6442bc1957d745b4a4374
wvongphanith30 <tankishootermk7@gmail.com>
2022-05-23 09:09:10 -0400

    debug

--- src/commands/message/owner/eval.js
+++ src/commands/message/owner/eval.js
@@ -46,7 +46,7 @@ module.exports = {
             status = "error";
         }
 
-        console.log(`${message.author.tag} ran code: ${code}`);
+        console.log(`${message.author.tag} ran code and got ${result} in ${ms(time2 - time1)}`);
 
         // If the result is a string, truncate it to 2000 characters.
         if (typeof result === "string") {
