86a1d5d689dda5f5b3f517ecd974e9ef054185b8
ThunderRedStar <wvongphanith30@stuy.edu>
2022-05-23 20:57:04 -0400

    dig

---
+++ src/commands/message/economy/dig.js
@@ -0,0 +1,66 @@
+// Bot dig command.
+
+const ItemDatabase = require("../../../structures/ItemDatabase");
+let randomArrayElement = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+let digPossible = [
+  {
+    "name": "Test Item",
+    "threshold": 1.00,
+  }
+];
+
+module.exports = {
+  botPermissions: ["SEND_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "USE_EXTERNAL_EMOJIS"],
+  userPermissions: [],
+  enabled: true,
+  guildOnly: false,
+  ownerOnly: false,
+  name: "dig",
+  aliases: ["d"],
+  description: "Dig for potatoes.",
+  detailedDescription: "Dig for potatoes in the ground, which are added to your inventory.",
+  cooldown: 10000,
+  args: [],
+  run: async function(message, client, args) {
+    // Check if the user already has an account.
+    let user = client.database.users.get(message.author.id);
+    if (!user) {
+      return message.reply({
+        embeds: [{
+          color: client.colors.warning,
+          description: client.customEmojis.warning + " You haven't started an account yet. Use `" + client.config.defaultPrefix + "start` to start one."
+        }]
+      });
+    } else {
+      // Dig for potatoes.
+      let randomThreshold = Math.random();
+      // Create the threshold array, based off the digPossible array.
+      let thresholdArray = [];
+      for (let i = 0; i < digPossible.length; i++) {
+        thresholdArray.push(digPossible[i].threshold);
+      }
+      // Find the index of the threshold that the random number is in between.
+      let thresholdIdx;
+      for (let i = 1; i < thresholdArray.length; i++) {
+        if (randomThreshold <= thresholdArray[i] && randomThreshold > thresholdArray[i - 1]) {
+          thresholdIdx = i;
+          break;
+        }
+      }
+      // Get the item that the random number is in between.
+      let item = digPossible[thresholdIdx];
+      // Add the item to the user's inventory.
+      user.inventory.add(item.name, 1);
+      // Send the user a message.
+      message.reply({
+        embeds: [{
+          color: client.colors.potato,
+          description: "You managed to dig up a " + item.name + " worth " + ItemDatabase.getItem(item.name).worth + " " + client.customEmojis.potato + "."
+        }]
+      });
+    }
+  }
+};
