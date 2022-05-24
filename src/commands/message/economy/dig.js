1ebe8a0fba666b651b64eac5422cd97f130d7077
ThunderRedStar <wvongphanith30@stuy.edu>
2022-05-23 20:58:06 -0400

    digwork

--- src/commands/message/economy/dig.js
+++ src/commands/message/economy/dig.js
@@ -51,7 +51,7 @@ module.exports = {
         }
       }
       // Get the item that the random number is in between.
-      let item = digPossible[thresholdIdx];
+      let item = digPossible[thresholdIdx - 1];
       // Add the item to the user's inventory.
       user.inventory.add(item.name, 1);
       // Send the user a message.
