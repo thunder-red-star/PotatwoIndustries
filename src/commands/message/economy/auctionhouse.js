109e1b069cf7566881e063337fa9f103e1c0d036
wvongphanith30 <tankishootermk7@gmail.com>
2022-05-25 09:30:27 -0400

    don't list expired auctions

--- src/commands/message/economy/auctionhouse.js
+++ src/commands/message/economy/auctionhouse.js
@@ -16,27 +16,37 @@ module.exports = {
     run: async function(message, client, args) {
         // List the auction house.
         let auctions = client.database.auctions.getAuctions();
-        let auctionPages = [];
-        // Put 10 auctions per page.
-        for (let i = 0; i < auctions.length; i += 10) {
-            let page = new DJSBuilders.Embed()
-                .setColor(client.colors.potato)
-                .setTitle("Auction House - Page " + Math.floor(i / 10) + 1)
-                .setDescription("Get an item with `" + client.getServerPrefix(message) + "ahbuy <id>`!\nLearn more about an auction with `" + client.getServerPrefix(message) + "ahinfo <id>`!");
-            for (let j = i; j < i + 10; j++) {
-                if (j >= auctions.length) break;
-                if (Date.now() - auction.time > 1000 * 24 * 60 * 60) {
-                    continue;
-                } else {
-                    let auction = auctions[j];
-                    page.addField({
-                        name: "**" + auction.count + "x " + auction.item.name + "**",
-                        value: "Id: `" + auction.id + "`",
-                    });
+        if (auctions.length < 1) {
+            return message.reply({
+                embeds: [{
+                    title: "Auction House",
+                    description: "There are no auctions right now.",
+                    color: client.colors.potato
+                }]
+            });
+        } else {
+            let auctionPages = [];
+            // Put 10 auctions per page.
+            for (let i = 0; i < auctions.length; i += 10) {
+                let page = new DJSBuilders.Embed()
+                    .setColor(client.colors.potato)
+                    .setTitle("Auction House - Page " + Math.floor(i / 10) + 1)
+                    .setDescription("Get an item with `" + client.getServerPrefix(message) + "ahbuy <id>`!\nLearn more about an auction with `" + client.getServerPrefix(message) + "ahinfo <id>`!");
+                for (let j = i; j < i + 10; j++) {
+                    if (j >= auctions.length) break;
+                    if (Date.now() - auction.time > 1000 * 24 * 60 * 60) {
+                        continue;
+                    } else {
+                        let auction = auctions[j];
+                        page.addField({
+                            name: "**" + auction.count + "x " + auction.item.name + "**",
+                            value: "Id: `" + auction.id + "`",
+                        });
+                    }
                 }
+                auctionPages.push(page);
             }
-            auctionPages.push(page);
+            await messagePaginator(message, auctionPages);
         }
-        await messagePaginator(message, auctionPages);
     }
 };
