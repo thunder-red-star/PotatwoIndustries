b51ea00cd40043c104958030731c4891a3d39a8a
wvongphanith30 <tankishootermk7@gmail.com>
2022-05-25 09:29:22 -0400

    don't list expired auctions

--- src/commands/message/economy/auctionhouse.js
+++ src/commands/message/economy/auctionhouse.js
@@ -25,11 +25,15 @@ module.exports = {
                 .setDescription("Get an item with `" + client.getServerPrefix(message) + "ahbuy <id>`!\nLearn more about an auction with `" + client.getServerPrefix(message) + "ahinfo <id>`!");
             for (let j = i; j < i + 10; j++) {
                 if (j >= auctions.length) break;
-                let auction = auctions[j];
-                page.addField({
-                    name: "**" + auction.count + "x " + auction.item.name + "**",
-                    value: "Id: `" + auction.id + "`",
-                });
+                if (Date.now() - auction.time > 1000 * 24 * 60 * 60) {
+                    continue;
+                } else {
+                    let auction = auctions[j];
+                    page.addField({
+                        name: "**" + auction.count + "x " + auction.item.name + "**",
+                        value: "Id: `" + auction.id + "`",
+                    });
+                }
             }
             auctionPages.push(page);
         }
